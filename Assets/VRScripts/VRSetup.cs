96a89f58bc134063c1f40d18e011e0338ec73f74
Brendan Ragan <lordmortis@gmail.com>
2016-01-29 22:19:37 +0800

    create some objects, realize we need HDR for glowy bits, add SMAA for some anti-aliasing that works with HDR
    Former-commit-id: e1ce5e12673a83dc0e1b93b2cae5c702e697efec

---
+++ Assets/VRScripts/VRSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRSetup : MonoBehaviour {
+
+    [SerializeField]
+    private float GearVRRenderScale = 1.0f;
+    [SerializeField]
+    private float PCRenderScale = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!vrSettingsDone)
+        {
+            UnityEngine.VR.VRSettings.renderScale = PCRenderScale;
+            vrSettingsDone = true;
+        }
+	}
+
+    private bool vrSettingsDone = false;
+}
