d865fd478f4525a47ad5b80c63656162f7f2198d
Brendan Ragan <lordmortis@gmail.com>
2016-02-22 17:55:37 +0800

    fix interactivity - only one object can now glow at a time.

--- Assets/VRScripts/VRMotionInteractive.cs
+++ Assets/VRScripts/VRMotionInteractive.cs
@@ -9,8 +9,10 @@ namespace VRStandardAssets.Utils
         public int controllerId;
 
         public SteamVR_TrackedObject viveTracker;
+        
+        private List<VRInteractiveItem> touchingItems = new List<VRInteractiveItem>();
+        private VRInteractiveItem lastInteracted = null;
         private VRInteractiveItem[] selfInteractible;
-        private VRInteractiveItem currentInteractible;
         private bool triggered;
         private Valve.VR.VRControllerState_t controllerState;
 
@@ -21,13 +23,31 @@ namespace VRStandardAssets.Utils
 
         public void Update()
         {
-            if (currentInteractible == null)
+            if (touchingItems.Count == 0)
             {
                 return;
             }
-            var interacting = isInteracting();
 
-            if (!triggered && interacting)
+            var currentInteractible = findCurrentInteractive();
+            if (currentInteractible == null)
+            {
+                Debug.LogWarning("Current Interactible is null but we have touching items?");
+                return;
+            }
+            
+            if (currentInteractible != lastInteracted)
+            {
+                if (lastInteracted != null)
+                {
+                    lastInteracted.Untouch();
+                }
+                currentInteractible.Touch();
+                lastInteracted = currentInteractible;
+            }
+            
+            var triggering = controllerIsTriggering();
+
+            if (!triggered && triggering)
             {
                 currentInteractible.TouchTrigger();
                 for (int i = 0; i < selfInteractible.Length; i++)
@@ -36,7 +56,7 @@ namespace VRStandardAssets.Utils
                 }
                 triggered = true;
             }
-            else if (triggered && !interacting)
+            else if (triggered && !triggering)
             {
                 currentInteractible.TouchTriggerStop();
                 for (int i = 0; i < selfInteractible.Length; i++)
@@ -58,13 +78,14 @@ namespace VRStandardAssets.Utils
             {
                 return;
             }
-            
-            currentInteractible = interactible;
-            interactible.Touch();
-            for (int i = 0; i < selfInteractible.Length; i++)
+
+            if (touchingItems.Contains(interactible))
             {
-                selfInteractible[i].Touch();
-            }            
+                return;
+            }
+
+            touchingItems.Add(interactible);
+            updateSelfInteractiveStates();
         }
 
         public void OnTriggerExit(Collider other)
@@ -79,18 +100,65 @@ namespace VRStandardAssets.Utils
                 return;
             }
 
-            if (currentInteractible == interactible)
+            if (!touchingItems.Contains(interactible))
             {
-                currentInteractible = null;
+                return;
             }
-            interactible.Untouch();
-            for (int i = 0; i < selfInteractible.Length; i++)
+
+            touchingItems.Remove(interactible);
+            updateSelfInteractiveStates();
+            if (touchingItems.Count == 0)
             {
-                selfInteractible[i].Untouch();
+                if (lastInteracted != null)
+                {
+                    lastInteracted.Untouch();
+                    lastInteracted = null;
+                }
             }
         }
 
-        private bool isInteracting()
+        private void updateSelfInteractiveStates() 
+        {
+            bool activeItem = touchingItems.Count > 0;
+            for (int i = 0; i < selfInteractible.Length; i++)
+            {
+                if (activeItem)
+                    selfInteractible[i].Touch();
+                else
+                    selfInteractible[i].Untouch();
+            } 
+        }
+
+        private VRInteractiveItem findCurrentInteractive()
+        {
+            var count = touchingItems.Count;
+            if (count == 0)
+            {
+                return null;
+            }
+
+            if (count == 1)
+            {
+                return touchingItems[0];
+            }
+
+            var distance = 1000.0f;
+            VRInteractiveItem closestItem = null;
+
+            foreach (var interactive in touchingItems)
+            {
+                var itemDistance = (interactive.transform.position - this.transform.position).sqrMagnitude;
+                if (itemDistance < distance)
+                {
+                    closestItem = interactive;
+                    distance = itemDistance;
+                }
+            }
+
+            return closestItem;
+        }
+
+        private bool controllerIsTriggering()
         {
 #if UNITY_PS4
             var buttonState = PS4Util.Move.currentButtonStates(playerId, controllerId);
